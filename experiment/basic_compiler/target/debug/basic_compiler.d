7f45ed19236efe06b9a297ebfc7facae9ef06440
clownfish733 <d@niel-g.com>
2026-07-31 02:46:39 +0100

    Delete experiment/basic_compiler/target directory

--- experiment/basic_compiler/target/debug/basic_compiler.d
+++
@@ -1,1 +0,0 @@
-/home/clownfish73/Projects/repos/compiler/experiment/basic_compiler/target/debug/basic_compiler: /home/clownfish73/Projects/repos/compiler/experiment/basic_compiler/src/main.rs
--- experiment/basic_compiler/target/debug/deps/basic_compiler-216ba2cf72f13415.d
+++
@@ -1,9 +0,0 @@
-/home/clownfish73/Projects/repos/compiler/experiment/basic_compiler/target/debug/deps/basic_compiler-216ba2cf72f13415.d: src/main.rs Cargo.toml
-
-/home/clownfish73/Projects/repos/compiler/experiment/basic_compiler/target/debug/deps/libbasic_compiler-216ba2cf72f13415.rmeta: src/main.rs Cargo.toml
-
-src/main.rs:
-Cargo.toml:
-
-# env-dep:CLIPPY_ARGS=
-# env-dep:CLIPPY_CONF_DIR
